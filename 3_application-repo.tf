ec9b5005359f97b2ddceb2e865fa5340c6732860
Andrey <56207582+kpkq@users.noreply.github.com>
2022-04-07 00:16:24 +0300

    Update 3_application-repo.tf

--- 3_application-repo.tf
+++ 3_application-repo.tf
@@ -2,7 +2,7 @@ resource "github_repository" "application" {
   name        = "application"
   description = "application repo"
 
-  visibility = "public"
+  visibility = "private"
   allow_merge_commit = false
   has_downloads = true
   has_issues = true
